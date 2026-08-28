87141ea7a4f8b156d5f911144d408c7cc91bbc7a
Sara Cipriano <130059301+SaraGuaiume@users.noreply.github.com>
2026-08-28 15:22:47 -0300

    .

--- sensorTemperatura/src/main.cpp
+++ sensorTemperatura/src/main.cpp
@@ -2,25 +2,33 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <Adafruit_Sensor.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
 #define sensorLuz A0
 #define sensorUmidade A1
 #define sensorTemperatura 2
 #define DHTTYPE DHT22
 
+LiquidCrystal_I2C lcd(0x27,16,2);
 DHT dht(sensorTemperatura, DHTTYPE);
 
-void lerTemperatura(){
+void lerTemperatura(LiquidCrystal_I2C lcd2){
   double temperatura = dht.readTemperature();
 
   Serial.print("Temperatura: ");
   Serial.print(temperatura);
   Serial.println("°C");
 
-  delay(1000);
+  lcd2.setCursor(0, 0); // coluna 0, linha 0
+  lcd2.print("T: ");
+  lcd2.print(temperatura);
+  lcd2.print("C");
+
+  delay(2000);
 }
 
-void lerLuminosidade(){
+void lerLuminosidade(LiquidCrystal_I2C lcd2){
   int valorluminosidade = analogRead(sensorLuz);
   long porcentagemLuminosidade = (long)valorluminosidade*100/1023;
   
@@ -28,27 +36,46 @@ void lerLuminosidade(){
   Serial.print(porcentagemLuminosidade);
   Serial.println('%');
 
-  delay(1000);
+  Serial.print("Luminosidade: ");
+  Serial.println(valorluminosidade);
+
+  lcd2.setCursor(0, 1); // coluna 0, linha 0
+  lcd2.print("L: ");
+  lcd2.print(porcentagemLuminosidade);
+  lcd2.print("%");
+
+  delay(2000);
 }
 
-void lerUmidade(){
+void lerUmidade(LiquidCrystal_I2C lcd2){
   int valorUmidade = analogRead(sensorUmidade);
-  float porcentagemUmidade = (1023 - valorUmidade) * 100.0 / 723.0;
+  long porcentagemUmidade = (1023 - valorUmidade) * 100.0 / 923.0;
 
   Serial.print("Umidade do solo: ");
   Serial.print(porcentagemUmidade);
   Serial.println("%");
+
+  Serial.print("valor umidade: ");
+  Serial.println(valorUmidade);
   Serial.println("============================================");
-  delay(1000);
+
+  lcd2.setCursor(8, 1); // coluna 0, linha 0
+  lcd2.print("U: ");
+  lcd2.print(porcentagemUmidade);
+  lcd2.print("%");
+
+  delay(2000);
 }
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  lerTemperatura();
-  lerLuminosidade();
-  lerUmidade();
+  lerTemperatura(lcd);
+  lerLuminosidade(lcd);
+  lerUmidade(lcd);
 }
